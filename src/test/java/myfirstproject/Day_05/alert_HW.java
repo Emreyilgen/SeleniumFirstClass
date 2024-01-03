2090068083e7ed6e9c6b22c0e1b599b54f7b5d80
Emre Yilgen <emreyilgen@gmail.com>
2024-01-03 20:42:17 +0300

    day06

---
+++ src/test/java/myfirstproject/Day_05/alert_HW.java
@@ -0,0 +1,17 @@
+package myfirstproject.Day_05;
+
+public class alert_HW {
+    /*
+    HW: When Navigate to https://testpages.herokuapp.com/styled/windows-test.html
+ØWhen Click on "Alerts In A New Window From JavaScript"
+ØAnd Switch to new window
+ØAnd Click on "Show alert box" button
+ØAnd Accept alert
+ØAnd Click on "Show confirm box" button
+ØAnd Cancel alert
+ØThen Assert that alert is cancelled
+ØWhen Click on "Show prompt box" button
+ØAnd Send "Hello World!" to the alert
+ØThen Assert that "Hello World!" is sent
+     */
+}
--- src/test/java/myfirstproject/Day_06/Day06_Iframes.java
+++ src/test/java/myfirstproject/Day_06/Day06_Iframes.java
@@ -8,41 +8,47 @@ import org.openqa.selenium.By;
 public class Day06_Iframes extends TestBase {
 
 
+ /*
+    NOTES:
+    - There are 3 ways to switch to frames : index, id or name, xpath
+         1. driver.switchTo().frame(1);  // by index
+         2. driver.switchTo().frame("left"); // by name or id
+         3. driver.switchTo().frame("//frame[@name='left']"); // by xpath
+    - When you want to switch to sibling frame (non-child), you need to switch to parent and then to the sibling frame
+
+     */
 
 
     @Test
-    public void iframeTest(){
-
-// Go to https://testpages.herokuapp.com/styled/frames/frames-test.html
-
-    driver.get("https://testpages.herokuapp.com/styled/frames/frames-test.html");
-
+    public void iframeTest() {
+//    Go to https://testpages.herokuapp.com/styled/frames/frames-test.html
+        driver.get("https://testpages.herokuapp.com/styled/frames/frames-test.html");
 //    Then Assert that "Left List Item 29" is the last element
-   //     Left list item 29 element is inside
-                driver.switchTo().frame(1);  // by index
-                //driver.switchTo().frame("left");  // by name or id
-                //driver.switchTo().frame("//frame[@name='left']");  // by xpath
-        //NOW WE ARE INSINE THE IFRAME
+//        "Left List Item 29" element is INSIDE an iframe. So we need to SWITCH TO IFRAME FIRST
+        driver.switchTo().frame(1);  // by index
+//        driver.switchTo().frame("left"); // by name or id
+//        driver.switchTo().frame("//frame[@name='left']"); // by xpath
 
-        //driver.findElement(By.id("left29")).isDisplayed();
-        //driver.findElement(By.id("//li[@id='left']"));  // hard way of testing if last item in the list is "list29"
-        String lastElement = driver.findElement(By.xpath("//li[last()]")).getText(); // last() return the last element everytime.
-        // this way the xpaht is more dynamic.
-        System.out.println("lastElement = " + lastElement);
+        // NOW WE ARE INSIDE THE IFRAME
+//        driver.findElement(By.id("left29"));
+//       driver.findElement(By.xpath("//li[@id='left29']")); // hard way of testing if the last item in the list is "left29"
+        String lastElement = driver.findElement(By.xpath("//li[last()]")).getText();
+        // last() returns the last element every time. This way the xpath is more dynamic
+        System.out.println(lastElement);
         Assertions.assertTrue(lastElement.equals("Left List Item 29"));
 
 
 //    And Assert that "Middle List Item 39" is the last element in the "Middle"
-     //   at thispoint, driver is inside the "left" frame we need to switch to parent frame
-    //            or default page level to switch ti the "middle" frame
-
-        driver.switchTo().parentFrame();
-        //driver.switchTo().defaultContent();
+//        At this point, driver is inside the "left" frame, we need to switch to parent frame
+//        or default page level to switch to the "middle" frame
+        driver.switchTo().parentFrame();  // OR
+//       driver.switchTo().defaultContent();
         driver.switchTo().frame("middle"); // switching to the middle frame
         // now locate the last item
-        String middleFrameFastItem = driver.findElement(By.xpath("//li[last()")).getText();
-        System.out.println("middleFrameFastItem = " + middleFrameFastItem);
-        Assertions.assertTrue(middleFrameFastItem.equals("Middle List Item 39"));
+        String middleFrameLastItem = driver.findElement(By.xpath("//li[last()]")).getText();
+        System.out.println("middleFrameLastItem = " + middleFrameLastItem);
+
+        Assertions.assertTrue(middleFrameLastItem.equals("Middle List Item 39"));
 
 
     }
